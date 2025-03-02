9336ba987db90ea74b1982198342371731b7d3e4
Mayank Agrawal <149233491+Mayank-agra@users.noreply.github.com>
2025-03-02 15:24:06 +0530

    Update dashboard.jsx

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./dashboard.css";
 import { CourseData } from "../../context/CourseContext";
-import CourseCard from "../../components/coursecard/CourseCard";
+import CourseCard from "../../components/courseCard/courseCard";
 
 const Dashbord = () => {
   const { mycourse } = CourseData();
