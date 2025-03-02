2fefea01f14b3afb8484c77cc225ad536b94898f
Mayank Agrawal <149233491+Mayank-agra@users.noreply.github.com>
2025-03-02 15:14:21 +0530

    Update courses.jsx

--- src/pages/courses/courses.jsx
+++ src/pages/courses/courses.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./courses.css";
 import { CourseData } from "../../context/CourseContext";
-import CourseCard from "../../components/coursecard/CourseCard";
+import CourseCard from "../../components/courseCard/courseCard";
 
 const Courses = () => {
   const { courses } = CourseData();
